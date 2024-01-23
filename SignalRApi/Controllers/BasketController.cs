898d0a4a67695bd13bd1bc40a8071d75007409b1
celalcolak94 <celalcolak94@gmail.com>
2024-01-24 01:13:57 +0300

    Menü sayfası oluşturuldu ve ürünler listelendi. Sepet sınıfı oluşturularak mimariye dahil edildi. Sepet ekranı tasarımı yapıldı ve sepetteki ürünler başarılı bir şekilde listelendi.

---
+++ SignalRApi/Controllers/BasketController.cs
@@ -0,0 +1,43 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using SignalR.BusinessLayer.Abstract;
+using SignalR.DtoLayer.BasketDto;
+
+namespace SignalRApi.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class BasketController : ControllerBase
+    {
+        private readonly IBasketService _basketService;
+
+        public BasketController(IBasketService basketService)
+        {
+            _basketService = basketService;
+        }
+
+        [HttpGet]
+        public IActionResult GetBasketByMenuTableID(int id)
+        {
+            var values = _basketService.TGetBasketByMenuTableNumber(id);
+            return Ok(values);
+        }
+
+        [HttpGet("BasketListByMenuTableWithProductName")]
+        public IActionResult BasketListByMenuTableWithProductName(int id)
+        {
+            var values = _basketService.TGetBasketByMenuTableNumber(id).Select(x => new ResultBasketListWithProductsDto
+            {
+                BasketID = x.BasketID,
+                Count = x.Count,
+                MenuTableID = x.MenuTableID,
+                Price = x.Price,
+                ProductID = x.ProductID,
+                ProductName = x.Product.ProductName,
+                TotalPrice = x.TotalPrice
+            }).ToList();
+
+            return Ok(values);
+        }
+    }
+}
--- SignalRWebUI/Controllers/MenuController.cs
+++ SignalRWebUI/Controllers/MenuController.cs
@@ -3,24 +3,24 @@ using Newtonsoft.Json;
 using SignalRWebUI.Dtos.CategoryDtos;
 using SignalRWebUI.Dtos.ProductDtos;
 
-namespace SignalRWebUI.ViewComponents.DefaultComponents
+namespace SignalRWebUI.Controllers
 {
-    public class _DefaultOurMenuPartialComponent : ViewComponent
+    public class MenuController : Controller
     {
         private readonly IHttpClientFactory _httpClientFactory;
 
-        public _DefaultOurMenuPartialComponent(IHttpClientFactory httpClientFactory)
+        public MenuController(IHttpClientFactory httpClientFactory)
         {
             _httpClientFactory = httpClientFactory;
         }
 
-        public async Task<IViewComponentResult> InvokeAsync()
+        public async Task<IActionResult> Index()
         {
             var client = _httpClientFactory.CreateClient();
 
             var responseMessage2 = await client.GetAsync("http://localhost:5125/api/Category");
             var jsonData2 = await responseMessage2.Content.ReadAsStringAsync();
-            var values2 = JsonConvert.DeserializeObject<List<ResultCategoryDto>> (jsonData2);
+            var values2 = JsonConvert.DeserializeObject<List<ResultCategoryDto>>(jsonData2);
             ViewBag.Categories = values2;
 
             var responseMessage = await client.GetAsync("http://localhost:5125/api/Product/ProductListWithCategory");
